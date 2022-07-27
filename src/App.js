ad651c2c312c9021b8e826a2a002ae66da35ee5a
cervusDev <gustavo.cervus@gmail.com>
2022-07-27 14:28:34 -0300

    refactor: rename imports

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 // just change the components commented to suit your needs
 
 import React from 'react'
-// import { DownloadDivs } from './components/useReactScreenshot'
-// import { DownloadCanvas } from './components/html2Canvas'
 import { Sheets } from './components/sheetsJs'
+// import { DownloadCanvas } from './components/html2Canvas'
+// import { DownloadSreenShots } from './components/useReactScreenshot'
 
 export function App() {
   const ref = React.useRef(null);
